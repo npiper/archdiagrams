763d175ab560dd270219d6b6ce0c6163f78ef475
npiper <solveapuzzledev@gmail.com>
2019-11-26 20:33:01 +0000

    Fixed some javadoc.

--- src/main/java/neilpiper/me/archdiagrams/ArchdiagramsApplication.java
+++ src/main/java/neilpiper/me/archdiagrams/ArchdiagramsApplication.java
@@ -3,9 +3,19 @@ package neilpiper.me.archdiagrams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Architecture diagrams SpringBoot application uses C4 Structurizr library.
+ * 
+ * @author neilpiper
+ *
+ */
 @SpringBootApplication
 public class ArchdiagramsApplication {
 
+  
+  /** Main method startup.
+   * 
+   * @param args startup args.O
+   */
   public static void main(String[] args) {
     SpringApplication.run(ArchdiagramsApplication.class, args);
   }
